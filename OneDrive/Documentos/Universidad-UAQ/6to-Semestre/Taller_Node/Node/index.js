70da676878ff345ac3396036e36ca4b6eafaf4bc
Jhovani Ramos <edgarjramos14@gmail.com>
2024-04-24 17:09:41 -0600

    Ruta myname

--- OneDrive/Documentos/Universidad-UAQ/6to-Semestre/Taller_Node/Node/index.js
+++ OneDrive/Documentos/Universidad-UAQ/6to-Semestre/Taller_Node/Node/index.js
@@ -7,6 +7,13 @@ app.get('/', (req, res)=>
     res.send('Hello world from port 3000')
 });
 
+app.get('/myname', (req, res) => {
+    
+    const name = req.query.name || 'Jhovani'; // Si no se proporciona un nombre, usa "NombreDesconocido" por defecto
+    
+    res.send(`Hello World! My name is ${name}.`);
+  });
+
 app.listen(3000, ()=>
 {
     console.log('La aplicación está escuchando en el puerto 3000...');
